eed0dd6011a691805b7bee66b3a579a76c68cb66
bgrados <65876480+bgrados@users.noreply.github.com>
2026-04-02 22:03:28 -0500

    supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -6,7 +6,7 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || ''
 
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || ''
 
-ok te voy pasando uno por uno hasta q termine de pasarte no me des respuesta si necesitas adicional me avisas
+
 
 if (!supabaseUrl || !supabaseKey) {
 
