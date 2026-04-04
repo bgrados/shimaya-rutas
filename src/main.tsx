980248cf9aab6ae8834b8c11a25e78ec69331d77
Ben Grados <bengrados@Peru-Mac-Mini.local>
2026-04-03 23:17:49 -0500

    Disable PWA temporarily to fix caching issues

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { registerSW } from 'virtual:pwa-register'
-
-registerSW({
-  immediate: true
-})
 
 const rootEl = document.getElementById('root')
 
