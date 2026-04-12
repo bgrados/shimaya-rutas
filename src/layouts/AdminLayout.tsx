4c8a2fcb7cd61d02df7fb5329c74c82e088fb307
Ben Grados <bengrados@Peru-Mac-Mini.local>
2026-04-11 22:00:28 -0500

    fix: mas padding mobile

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -103,8 +103,8 @@ export const AdminLayout: React.FC = () => {
         <div className="lg:hidden fixed inset-0 z-30 bg-black/50" onClick={() => setIsSidebarOpen(false)} />
       )}
 
-      {/* Main content - with left margin for sidebar */}
-      <div className="flex-1 flex flex-col min-h-screen lg:ml-64 pt-16 lg:pt-0">
+      {/* Main content - with left margin for sidebar - mas padding en mobile */}
+      <div className="flex-1 flex flex-col min-h-screen lg:ml-64 pt-20 lg:pt-0">
         <main className="flex-1 p-6 lg:p-8 overflow-y-auto">
           <div className="max-w-7xl mx-auto">
             <Outlet />
