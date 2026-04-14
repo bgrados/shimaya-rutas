c0b8e17adf37ef0847857c4770a5747e9409798b
Ben Grados <bengrados@Peru-Mac-Mini.local>
2026-04-14 13:29:47 -0500

    Fix Vercel build, add analisis

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      maximumFileSizeToCacheInBytes: 3000000,
       manifest: {
         name: 'Shimaya Rutas',
         short_name: 'Rutas',
